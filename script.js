d0433729ad76903c61010d225432bebdec6399f3
Victor Ivanov <justicejesus1237@gmail.com>
2019-03-02 20:32:12 +0300

    lesson2_hard done

---
+++ script.js
@@ -0,0 +1,41 @@
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+// document.write(week.join("<br>"));
+
+
+
+// if (i === week[5] && i === week[6])
+    br = "<br>",
+    b = "<b>";
+    em = "<em>"
+
+
+
+// for (let i = 0; i < week.length; i++) {
+  // let sat = week[4],
+  //     sun = week[5];
+    // week[5] = "<b>",
+    // week[6] = "<b>"
+  document.body.innerHTML = em + week[0] + br;
+  document.body.innerHTML += week[1] + br;
+  document.body.innerHTML += week[2] + br;
+  document.body.innerHTML += week[3] + br;
+  document.body.innerHTML += week[4] + br;
+  document.body.innerHTML += b + week[5] + br;
+  document.body.innerHTML += b + week[6] + br;
+  
+// }
+
+let arr = ["3432235", "45454", "5454789", "75454", "89089008", "3121233", "890898098"]
+
+console.log(arr[0], arr[3], arr[5])
+
+
+
+
+// for (let i = 0; i < week.length; i++) {
+//   if (i === week.length[4] && i === week.length[5]) {
+//     console.log("done");
+//   }
+// }
+
